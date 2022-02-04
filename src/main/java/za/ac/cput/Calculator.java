fcbf8bf6528ddaf7c472dec2ac0dd9f59f42e878
CharlesLemmert <81480730+CharlesLemmert@users.noreply.github.com>
2022-02-04 15:27:13 +0200

    Added multiplication method and test method

--- src/main/java/za/ac/cput/Calculator.java
+++ src/main/java/za/ac/cput/Calculator.java
@@ -9,6 +9,7 @@ public class Calculator
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
     }
 
     public int Subtraction(int first, int last){
@@ -19,6 +20,10 @@ public class Calculator
             ans  = last - first;
         }
 
+        return ans;
+    }
+    public int Multiplication(int first, int last){
+        int ans = first * last;
         return ans;
     }
 }
